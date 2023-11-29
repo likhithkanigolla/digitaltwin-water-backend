75b63276aafb065d77e2e56fe1e66b6ccaee0cff
Likhith Kanigolla <likhithkanigolla@gmail.com>
2023-11-29 16:11:25 +0530

    part Working

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
@@ -7,29 +8,39 @@ const PORT = 2345;
 app.use(express.json());
 app.use(cors());
 
-let waterUnitsAtA = Array(10).fill(1); // Initial water units at point A
-let waterUnitsAtB = []; // Initial water units at point B
+let waterUnitsAtA = Array(10).fill(1);
+let waterUnitsAtB = [];
+let waterUnitsAtC = [];
+let waterUnitsAtD = [];
 
-app.post('/startSimulation', (req, res) => {
-  res.status(200).json({ message: 'Simulation started' });
-});
-
-app.post('/stopSimulation', (req, res) => {
-  res.status(200).json({ message: 'Simulation stopped' });
-});
+app.use(bodyParser.json());
 
 app.post('/calculateAmount', (req, res) => {
   const { waterUnitsAtA: unitsAtA } = req.body;
   waterUnitsAtA = unitsAtA;
-  waterUnitsAtB = Array(10 - unitsAtA.length).fill(1); // Fill water units at point B accordingly
 
-  // Calculate the total amount at A and B
+  // Simulate water flow to tanks B, C, and D
+  waterUnitsAtB.push(...unitsAtA.splice(0, Math.min(unitsAtA.length, 1)));
+  waterUnitsAtC.push(...unitsAtA.splice(0, Math.min(unitsAtA.length, 1)));
+  waterUnitsAtD.push(...unitsAtA.splice(0, Math.min(unitsAtA.length, 1)));
+
   const calculatedAmountAtA = waterUnitsAtA.length;
   const calculatedAmountAtB = waterUnitsAtB.length;
+  const calculatedAmountAtC = waterUnitsAtC.length;
+  const calculatedAmountAtD = waterUnitsAtD.length;
 
-  res.status(200).json({ waterUnitsAtA, waterUnitsAtB, calculatedAmountAtA, calculatedAmountAtB });
+  res.json({
+    waterUnitsAtA,
+    waterUnitsAtB,
+    waterUnitsAtC,
+    waterUnitsAtD,
+    calculatedAmountAtA,
+    calculatedAmountAtB,
+    calculatedAmountAtC,
+    calculatedAmountAtD,
+  });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
